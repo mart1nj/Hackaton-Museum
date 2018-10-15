f228d2c6b6cd9b702d3ca62ab1b27a78367a68b2
Martin Joonas Pariis <mapari@ttu.ee>
2018-10-15 11:27:45 +0300

    Added GeoID & EmailID to ClientFactory.

--- Open/Domain/Bank/ClientFactory.cs
+++ Open/Domain/Bank/ClientFactory.cs
@@ -7,7 +7,8 @@ namespace Open.Domain.Bank
 {
     public static class ClientFactory
     {
-        public static Client CreateClient(string id, string firstName, string lastName, string passwordHash, DateTime? validFrom = null, DateTime? validTo = null)
+        public static Client CreateClient(string id, string firstName, string lastName, string passwordHash, string geographicID, string emailID,
+            DateTime? validFrom = null, DateTime? validTo = null)
         {
             var r = new ClientData
             {
@@ -15,6 +16,8 @@ namespace Open.Domain.Bank
                 FirstName = firstName,
                 LastName = lastName,
                 //PasswordHash = passwordHash,
+                GeographicAddressDataID = geographicID,
+                EmailAddressDataID = emailID,
                 ValidFrom = validFrom?? DateTime.MinValue,
                 ValidTo = validTo?? DateTime.MaxValue
             };
@@ -28,6 +31,8 @@ namespace Open.Domain.Bank
                 FirstName = client.FirstName,
                 LastName = client.LastName,
                 //PasswordHash = passwordHash,
+                GeographicAddressDataID = client.GeographicAddressDataID,
+                EmailAddressDataID = client.EmailAddressDataID,
                 ValidFrom = client.ValidFrom,
                 ValidTo = client.ValidTo
             };
