936fc9402846fb0bde5932645f7e6cb3df355256
Birgit Kruus <bikruu@ttu.ee>
2018-10-16 19:35:45 +0300

    added Account

---
+++ Open/Infra/Bank/AccountRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Open.Core;
+using Open.Data.Bank;
+using Open.Domain.Bank;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Open.Infra.Bank
+{
+    public class AccountRepository : Repository<Account, AccountData>, IAccountsRepository
+    {
+        public AccountRepository(SentryDbContext c) : base(c?.Account, c) { }
+
+        protected internal override Account createObject(AccountData r) { return new Account(r); }
+
+        protected internal override PaginatedList<Account> createList(List<AccountData> l,
+            RepositoryPage p)
+        {
+            return new AccountList(l, p);
+        }
+
+        protected internal override async Task<AccountData> getObject(string id)
+        {
+            return await dbSet.AsNoTracking()
+                .SingleOrDefaultAsync(x => x.ID == id);
+        }
+    }
+}
