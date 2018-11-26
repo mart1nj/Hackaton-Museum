84648e85b0a3c44666d2b81f3fe4dc55fca4e95a
stleeg <Stella.leego@gmail.com>
2018-11-26 23:16:10 +0200

    Ei saanud tööle programmi

---
+++ Open/Data/Bank/RequestTransactionData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Open.Core;
+
+namespace Open.Data.Bank
+{
+    public class RequestTransactionData : TransactionData
+    {
+        public Status Status { get; set; }
+    }
+}
