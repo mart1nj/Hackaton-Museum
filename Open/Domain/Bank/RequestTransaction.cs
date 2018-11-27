c469836f74c2372a36db65740f90c389f6992e4a
stleeg <Stella.leego@gmail.com>
2018-11-27 16:27:03 +0200

    bank domain

---
+++ Open/Domain/Bank/RequestTransaction.cs
@@ -0,0 +1,23 @@
+﻿using Open.Core;
+using Open.Data.Bank;
+using Open.Domain.Common;
+
+namespace Open.Domain.Bank
+{
+    public sealed class RequestTransaction : Entity<RequestTransactionData>
+    {
+        public readonly Account SenderAccount;
+        public readonly Account ReceiverAccount;
+        public readonly Status Status;
+        public RequestTransaction(RequestTransactionData data) : base(data)
+        {
+            SenderAccount = new Account(data?.SenderAccount);
+            ReceiverAccount = new Account(data?.ReceiverAccount);
+            if (data?.Status != null)
+            {
+                Status = data.Status;
+            }
+        }
+    }
+}
+}
