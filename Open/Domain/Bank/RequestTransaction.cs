23e3ede285a9540dfa957fa00970b1374a82377a
stleeg <Stella.leego@gmail.com>
2018-11-29 00:08:18 +0200

    Transaction stuff

--- Open/Domain/Bank/RequestTransaction.cs
+++ Open/Domain/Bank/RequestTransaction.cs
@@ -1,18 +1,13 @@
 ﻿using Open.Core;
 using Open.Data.Bank;
-using Open.Domain.Common;
 
 namespace Open.Domain.Bank
 {
-    public sealed class RequestTransaction : Entity<RequestTransactionData>
+    public sealed class RequestTransaction : Transaction
     {
-        public readonly Account SenderAccount;
-        public readonly Account ReceiverAccount;
         public readonly Status Status;
         public RequestTransaction(RequestTransactionData data) : base(data)
         {
-            SenderAccount = new Account(data?.SenderAccount);
-            ReceiverAccount = new Account(data?.ReceiverAccount);
             if (data?.Status != null)
             {
                 Status = data.Status;
---
+++ Open/Facade/Bank/RequestTransacionView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Open.Core;
+using Open.Facade.Bank;
+
+namespace Open.Facade.Bank
+{
+    public class RequestTransacionView : TransactionView
+    {
+        public Status status { get; set; }
+    }
+}
