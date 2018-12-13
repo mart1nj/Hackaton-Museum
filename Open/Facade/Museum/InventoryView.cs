045172b5422e0c7405cabda80145b6e6d0b59c5e
Birgit Kruus <bikruu@ttu.ee>
2018-12-13 21:16:46 +0200

    hardcoded values

--- Open/Facade/Museum/InventoryView.cs
+++ Open/Facade/Museum/InventoryView.cs
@@ -5,12 +5,28 @@ namespace Open.Facade.Museum
     public class InventoryView : EntityView
     {
         private string employee;
+        private string location;
+        private string department;
 
-        [DisplayName("Töötaja")]
+        [DisplayName("Komisjoni liige")]
         public string Employee
         {
             get => getString(ref employee);
             set => employee = value;
+        }
+        
+        [DisplayName("Asukoht")]
+        public string Location
+        {
+            get => getString(ref location);
+            set => location = value;
+        }
+        
+        [DisplayName("Muusemikogu")]
+        public string Department
+        {
+            get => getString(ref department);
+            set => department = value;
         }
     }
 }
